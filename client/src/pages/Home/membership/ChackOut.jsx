097dbd0d5fa32fdc4c994d7bc11f4544319c56e0
mahedi066 <imam25349@gmail.com>
2024-06-06 18:18:23 +0600

    basic strype payment method setup

--- client/src/pages/Home/membership/ChackOut.jsx
+++ client/src/pages/Home/membership/ChackOut.jsx
@@ -1,15 +1,20 @@
 import { Link, useLoaderData } from "react-router-dom";
 import UseAuth from "../../../hooks/UseAuth";
 import MyButton from "../../../components/MyButton";
-
+import {loadStripe} from '@stripe/stripe-js';
+import {Elements} from "@stripe/react-stripe-js"
+import ChackOutForm from "./ChackOutForm";
+// TODO : ADD PUBLISHABLE KEY
+const stripePromise =loadStripe(import.meta.env.VITE_PAYMENT_GETWAY_PK)
 
 const ChackOut = () => {
     const {user}=UseAuth()
     const {data}=useLoaderData()
     const {price,name,_id}=data
     return (
+        <Elements stripe={stripePromise}>
         <div className="mt-28">
-           <div className="lg:w-1/2 md:w-1/2 w-full px-4 mx-auto  h-screen space-y-2 ">
+           <div className="lg:w-1/2 md:w-1/2 w-full px-4 mx-auto   space-y-2 ">
             <h3 className="text-center font-bold text-xl pb-2">purchase monthly membership</h3>
             <hr />
             <p className="">order summary</p>
@@ -30,16 +35,19 @@ const ChackOut = () => {
             <p type="text" className="border-2 border-slate-200-400 w-full p-2">
                 {_id}
             </p>
+            <br />
             <strong>Payment Details</strong>
+            <ChackOutForm/>
             {/* stripe payment mehode  */}
 
                 <div className="text-center space-y-4">
                     <p className="font-semibold">secure payments with <Link className="text-blue-600" target="_blank"  to={'https://stripe.com/'}>stripe</Link></p>
                     <p>By providing your payment details, you authorise us to send instructions to the financial institution that issued your card to take payments from your card account.</p>
                 </div>
-            <MyButton toLink={''} label={'purchase'}/>
+            
            </div>
         </div>
+          </Elements>
     );
 };
 
