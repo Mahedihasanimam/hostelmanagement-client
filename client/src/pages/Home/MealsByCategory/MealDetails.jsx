4ffc2575d48ed26a2db6a47f50f12ffc032f1d12
mahedi066 <imam25349@gmail.com>
2024-06-16 10:24:41 +0600

    requested meal all functionality added

--- client/src/pages/Home/MealsByCategory/MealDetails.jsx
+++ client/src/pages/Home/MealsByCategory/MealDetails.jsx
@@ -11,17 +11,15 @@ import Swal from "sweetalert2";
 import MealReview from "../../../components/mealreview/MealReview";
 import ReviewCard from "../../../components/mealreview/ReviewCard";
 
-
 const MealDetails = () => {
   const { user } = UseAuth();
   const data = useLoaderData();
   const axiosCommon = useAxiosCommon();
-  const [mealreview,isLoading,refetch]=MealReview()
+  const [mealreview, isLoading, refetch] = MealReview();
 
-
-  const [likeCount,setLikeCount]=useState(0)
+  const [likeCount, setLikeCount] = useState(0);
   console.log(likeCount);
- 
+
   const {
     title,
     image,
@@ -32,18 +30,18 @@ const MealDetails = () => {
     distributor,
     ingredients,
     post_time,
-    _id
+    _id,
   } = data.data;
 
   const handlereview = async (e) => {
     e.preventDefault();
-    
+
     const review = e.target.review.value;
-    const reviewId=_id;
+    const reviewId = _id;
     const email = user.email;
     const name = user.displayName;
     const photo = user.photoURL;
-    const date=new Date().toLocaleDateString()
+    const date = new Date().toLocaleDateString();
 
     const mealreviewData = {
       likeCount,
@@ -53,54 +51,58 @@ const MealDetails = () => {
       name,
       photo,
       reviewId,
-      date
+      date,
     };
     console.table(mealreviewData);
-    const {data}=await axiosCommon.post('/mealreview',mealreviewData)
-    if(data.acknowledged){
+    const { data } = await axiosCommon.post("/mealreview", mealreviewData);
+    if (data.acknowledged) {
       Swal.fire({
         position: "top-end",
         icon: "success",
         title: "yor feedback added!!",
         showConfirmButton: false,
-        timer: 1500
+        timer: 1500,
       });
       e.target.review.value = "";
-      refetch()
+      refetch();
     }
-    
   };
-  const handleLikeCount=async()=>{
-    setLikeCount(likeCount+1);
+  const handleLikeCount = async () => {
+    setLikeCount(likeCount + 1);
 
-    const {data}=await axiosCommon.put(`/meals`,likeCount)
+    const { data } = await axiosCommon.put(`/meals`, likeCount);
     console.log(data);
-  }
+  };
 
-  const handlemealreauest=async(e)=>{
-    
-// console.log('hidded',e);
-// Swal.fire({
-//   title: "Are you sure?",
-//   text: "the mill will be added to request meal!",
-//   icon: "warning",
-//   showCancelButton: true,
-//   confirmButtonColor: "#3085d6",
-//   cancelButtonColor: "#d33",
-//   confirmButtonText: "Yes, Add it!"
-// }).then(async(result) => {
-
-//   if (result.isConfirmed) {
-//     Swal.fire({
-//       title: "Added!",
-//       text: "Your meal has been Added.",
-//       icon: "success"
-//     });
-//   }
-// });
-const {data}=await axiosCommon.post('/requestmeal',e)
-console.log(data);
-}
+  const handlemealreauest = async (e) => {
+    const mealReqData={
+      like:likeCount,
+      email:user?.email,
+      e
+    }
+   
+    Swal.fire({
+      title: "Are you sure?",
+      text: "the mill will be added to request meal!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, Add it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const { data } = await axiosCommon.post("/requestmeal", mealReqData);
+        if (data.acknowledged) {
+          Swal.fire({
+            title: "Added!",
+            text: "Your meal has been Added.",
+            icon: "success",
+          });
+        }
+      }
+      
+    });
+  };
   return (
     <div className="mt-28 ">
       <div className="">
@@ -111,26 +113,24 @@ console.log(data);
           <div className="card-body lg:w-1/2 mx-auto">
             <div className=" flex justify-between items-center ">
               <strong>category-{category}</strong>
-              <strong>review count-{mealreview.filter(i=>i.reviewId===_id).length}</strong>
+              <strong>
+                review count-
+                {mealreview.filter((i) => i.reviewId === _id).length}
+              </strong>
 
-              <button  disabled={!user}>
+              <button disabled={!user}>
                 <BiLike
-                  
                   onClick={() => {
-                 handleLikeCount()
-                    
+                    handleLikeCount();
                   }}
                   className={`cursor-pointer  lg:mr-8 ${
                     likeCount ? "text-blue-500 disabled" : ""
-                  } ${!user&&"cursor-not-allowed"}`}
+                  } ${!user && "cursor-not-allowed"}`}
                   size={70}
                 />
-                 <p>Like {likeCount}</p>
-                 {
-                  !user&& <p>login first to like this</p>
-                 }
+                <p>Like {likeCount}</p>
+                {!user && <p>login first to like this</p>}
               </button>
-             
             </div>
             <div className=" space-y-3">
               <h3 className="font-bold lg:text-4xl md:text-2xl text-xl">
@@ -162,33 +162,39 @@ console.log(data);
                   <strong>details</strong> : {details}
                 </p>
               </div>
-              <button onClick={()=>handlemealreauest(data.data)} disabled={!user} className="btn bg-blue-500 text-white font-bold mt-4 hover:bg-blue-500">
-             Request Meal
+              <button
+                onClick={() => handlemealreauest(data.data)}
+                disabled={!user}
+                className="btn bg-blue-500 text-white font-bold mt-4 hover:bg-blue-500"
+              >
+                Request Meal
               </button>
 
               <div className="border-2 border-slate-200 m-4 rounded-lg p-4">
-                {
-                  mealreview.filter(i=>i.reviewId===_id).map(item=><ReviewCard key={item._id} item={item}/>)
-                }
-              
-              <form onSubmit={handlereview}>
-                <textarea
-                  className="w-full mt-4 border-2 border-slate-300 rounded-md textarea textarea-info p-4"
-                  name="review"
-                  id=""
-                  cols="10"
-                  rows="5"
-                  placeholder="write your feedback"
-                ></textarea>
-                {/* <input>
+                {mealreview
+                  .filter((i) => i.reviewId === _id)
+                  .map((item) => (
+                    <ReviewCard key={item._id} item={item} />
+                  ))}
+
+                <form onSubmit={handlereview}>
+                  <textarea
+                    className="w-full mt-4 border-2 border-slate-300 rounded-md textarea textarea-info p-4"
+                    name="review"
+                    id=""
+                    cols="10"
+                    rows="5"
+                    placeholder="write your feedback"
+                  ></textarea>
+                  {/* <input>
                 <MyButton label={'send'}/>
                 </input> */}
-                <input
-                  className="btn bg-[#5070F3] hover:bg-[#5070F3] text-white font-bold px-8 "
-                  type="submit"
-                  value="send"
-                />
-              </form>
+                  <input
+                    className="btn bg-[#5070F3] hover:bg-[#5070F3] text-white font-bold px-8 "
+                    type="submit"
+                    value="send"
+                  />
+                </form>
               </div>
             </div>
           </div>
--- client/src/pages/dashboard/userMenu/requestedmeals/RequestedMeal.jsx
+++ client/src/pages/dashboard/userMenu/requestedmeals/RequestedMeal.jsx
@@ -1,11 +1,116 @@
+import { useQuery } from "@tanstack/react-query";
+import { axiosCommon } from "../../../../hooks/UseAxiosCommon";
 
-
+import { FaSkullCrossbones } from "react-icons/fa6";
+import Swal from "sweetalert2";
+import UseAuth from "../../../../hooks/UseAuth";
 const RequestedMeal = () => {
-    return (
-        <div>
-            <h1>requested meals</h1>
-        </div>
-    );
+  const {user}=UseAuth()
+  const { data: reqMeal = [],refetch } = useQuery({
+    queryKey: ["reqmeal"],
+    queryFn: async () => {
+      const { data } = await axiosCommon(`/requestedmeal`);
+      return data;
+    },
+  });
+  // console.log(reqMeal.e?.email);
+const myReqMeal=reqMeal.filter(i=>i.email===user?.email)
+  const handleRequestMealDelete = (id) => {
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to recover this query!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+      })
+      
+      .then(async(result) => {
+        // const {data}=await axiosCommon.delete(`/requestmeal/${id}`)
+        // console.log(data);
+        if (result.isConfirmed) {
+          Swal.fire({
+            title: "Deleted!",
+            text: "Your query has been deleted.",
+            icon: "success"
+          });
+          try{
+            const {data}= await axiosCommon.delete(`${import.meta.env.VITE_API_URL}/requestmeal/${id}`)
+            refetch()
+          }
+           catch(err){
+            console.log(err)
+           }
+        }
+      });
+  };
+
+  return (
+    <div>
+      <div className="overflow-x-auto min-h-screen my-8 px-4">
+        <table className="table">
+          {/* head */}
+          <thead className="bg-blue-100 text-black">
+            <tr>
+              <th>meal title</th>
+              <th>Liks</th>
+              <th>Review Details</th>
+              <th>Status</th>
+              <th>email</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {myReqMeal.map((reco) => (
+              
+              <tr className="even:text-black" key={reco._id}>
+               
+
+                <td>{reco.e?.title}</td>
+                <td>{reco.like}</td>
+                <td>{reco.e?.details?.slice(0, 50)}</td>
+                <td>pending</td>
+                <td>{reco.email}</td>
+                <th>
+                  <button
+                    onClick={() => handleRequestMealDelete(reco._id)}
+                    className="btn btn-ghost"
+                  >
+                    <FaSkullCrossbones size={25} />
+                  </button>
+                 
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        {myReqMeal.length < 1 && (
+          <div className="flex my-8 mx-auto  flex-row px-4 lg:w-[1000PX]  pl-4 py-2 gap-2 items-center border rounded-lg shadow overflow-hidden bg-gray-900 dark:bg-gray-50 border-violet-400 dark:border-violet-600">
+            <span className="flex-shrink-0 inline-flex mx-3 item-center justify-center leading-none rounded-full bg-violet-400 dark:bg-violet-600 text-gray-900 dark:text-gray-50">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+                className="h-8 w-8"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </span>
+            <div className="flex-1  p-2 ">
+              <h1 className="lg:text-4xl text-xl font-bold text-white">
+                Ooops! No Requested meal yeat{" "}
+              </h1>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default RequestedMeal;
